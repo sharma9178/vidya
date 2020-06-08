caf29be8296facd4aa487a8182ec5e78344489b2
sharma9178 <bsp9178@gmail.com>
2020-06-08 13:22:36 +0530

    initial

---
+++ footer.php
@@ -0,0 +1,93 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+	<!-- Css Stylesheet-->
+	<link href="css/style.css"  rel="stylesheet">
+	
+	<!--font awesome-->
+     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <title>HomePage</title>
+  </head>
+  <body>
+
+
+
+<!--FOOTER SECTION -->
+<footer style="background-color:green;">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">college road,Bargarh, Odisha</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+91-9439209361</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">contact@vidyasagaracademybrg.in</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+           
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5 ml-md-4">
+              <h2 class="ftco-heading-2">Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home Top</a></li>
+                <li><a href="fees.php"><span class="ion-ios-arrow-round-forward mr-2"></span>Fees</a></li>
+                <li><a href="services.php"><span class="ion-ios-arrow-round-forward mr-2"></span>Tc Zone</a></li>
+                <li><a href="downloads.php"><span class="ion-ios-arrow-round-forward mr-2"></span>Downloads</a></li>
+                <li><a href="contact.php"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5">
+            	<h2 class="ftco-heading-2">Subscribe Us!</h2>
+              <form action="#" class="subscribe-form">
+                <div class="form-group">
+                  <input type="text" class="form-control mb-2 text-center" placeholder="Enter email address">
+                  <input type="submit" value="Subscribe" class="form-control submit px-3">
+                </div>
+              </form>
+            </div>
+            <div class="ftco-footer-widget mb-5">
+            	<h2 class="ftco-heading-2 mb-0">Connect With Us</h2>
+            	<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-ce
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |  <i class="icon-heart" aria-hidden="true"></i>  <a href="#" target="_blank"></a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+	
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,116 @@
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+	<!-- Css Stylesheet-->
+	<link href="css/style.css"  rel="stylesheet">
+	
+	<!--font awesome-->
+     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <title>HomePage</title>
+  </head>
+  <body>
+ <div class ="container-fluid top-color">
+		<div class="row"style="position:relative;top:10px;">
+			<div class="col-lg-4 col-md-4 col-sm-12">
+				<p class="top-display"><i class="fa fa-globe" aria-hidden="true" style="position:relative;right:10px;"></i>College Road, Bargarh,Odisha</p>
+		    </div>
+			
+			<div class="col-lg-4 col-md-4 col-sm-12">
+				<p class="top-display"><i class="far fa-envelope-open" style="position:relative;right:10px;"></i>contact@vidyasagaracademybrg.in</p>
+		    </div>
+			<div class="col-lg-4 col-md-4 col-sm-12">
+				<p class="top-display"><i class="fas fa-phone-volume" style="position:relative;right:10px;"></i>+91-9439209361, 8895416410</p>
+		    </div>
+		</div>
+</div>
+	
+	
+
+
+
+<div class="sticky-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" style="font-size:30px;" href="index.php"><img src="logo.png" style="width:50px; height:60px;" alt="logo">VIDYASAGR ACADEMY</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+				<a class="nav-link" href="index.php" id="list" >Home <span class="sr-only">(current)</span></a>
+			</li>
+
+			<li class="nav-item dropdown">
+				<a class="nav-link" id="list">Overview</a>
+					<div class="dropdown-content">
+						<a href="about-us.php">About us</a>
+						<a href="mission-vision.php">Mission & Vision</a>
+						<a href="we-believe.php">We Believe</a>
+						<a href="chairman.php">Chairman Message</a>
+						<a href="md-message.php">Managing Director Message</a>
+						<a href="pri-message.php">Principal Message</a>
+						<a href="ad-message.php">Academic Director Message</a>
+					    <a href="faculty.php">Faculty</a>
+					</div>
+			</li>
+	  
+	  
+			<li class="nav-item dropdown" >
+	     		 <a class="nav-link" id="list">Admission</a>
+					<div class="dropdown-content">
+						<a href="academics.php">Academics</a>
+						<a href="school-brochure.php">School Brochure</a>
+						<a href="kids.php">Kids</a>
+						<a href="primary.php">Primary (Class I-V)</a>
+						<a href="secondary.php">Secondary(Class VI-X)</a>
+						<a href="senior-secondary.php">Senior Secondary (+2 Sc & Comm)</a>
+					</div>
+			</li>
+		
+			<li class="nav-item">
+				<a class="nav-link" id="list" href="index.php" >Gallery <span class="sr-only">(current)</span></a>
+			</li>
+	 
+			<li class="nav-item">
+				<a class="nav-link"id="list"  href="index.php" >News & Events </a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" id="list" href="index.php" >Downloads</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" id="list" href="index.php" >Contact us </a>
+			</li>
+    </ul>
+    
+  </div>
+</nav>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>			
